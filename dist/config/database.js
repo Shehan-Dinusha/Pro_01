cf45a4e8b664380e579ba2adef1393bb8351681b
Shehan-Dinusha <136222997+Shehan-Dinusha@users.noreply.github.com>
2024-01-11 18:24:58 +0530

    Delete dist directory

--- dist/config/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('crud_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-exports.default = db;
